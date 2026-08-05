a5fcfb0f265d837eaac68250fc598bb067ad1e6a
francisnicholasramos <pfrancisnicholasramos@gmail.com>
2026-08-06 00:25:05 +0800

    Modify Treesitter setup and configuration
    Updated Treesitter configuration to include new languages and changed event handling.

--- nvim/lua/lazy_plugins/treesitter.lua
+++ nvim/lua/lazy_plugins/treesitter.lua
@@ -1,30 +1,12 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
-        run = ":TSUpdate",
-        event = "BufRead",
+        branch = 'main',
+        build = ":TSUpdate",
+        lazy = false,
         config = function()
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = {
-                    "typescript", "javascript", "bash", "html"
-                },
-
-                highlight = { 
-                    enable = false,
-
-                    disable = function(lang, buf)
-                        local max_filesize = 20 * 1024 -- 100 KB
-                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                        if ok and stats and stats.size > max_filesize then
-                            return true
-                        end
-                    end,
-                },
-
-                indent = { 
-                    enable = true, 
-                    disable = { "html" } 
-                },
+            require("nvim-treesitter").install({
+                'html', 'blade'
             })
         end,
     },
